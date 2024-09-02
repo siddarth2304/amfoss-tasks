b37c4870ff028b307a0b81d2216880bea4e5b864
Sahith Siddarth <160704351+siddarth2304@users.noreply.github.com>
2024-09-02 15:06:26 +0530

    Create Sub-Task 3.go

---
+++ task-3/Go/Sub-Task 3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    for i := 0; i < n; i++ {
+        fmt.Println(strings.Repeat(" ", n-i-1) + strings.Repeat("*", 2*i+1))
+    }
+    for i := n - 2; i >= 0; i-- {
+        fmt.Println(strings.Repeat(" ", n-i-1) + strings.Repeat("*", 2*i+1))
+    }
+}
